e7a18b1580a803ec7878a90ca7d91e8521286271
Iman-Fatima0 <iman.f8094@gmail.com>
2024-11-04 01:02:40 +0500

    commit

--- Index.js
+++ Index.js
@@ -1,16 +1,18 @@
 const express = require('express')
 const app = express()
+
+app.use(express.json());
 const Car=[
-    { name: 'Mercedes', model: 'C-Class', make: 'Mercedes-Benz', year: 2022 },
-    { name: 'BMW', model: '3 Series', make: 'BMW', year: 2021 },
-    { name: 'Audi', model: 'A4', make: 'Audi', year: 2023 },
-    { name: 'Toyota', model: 'Camry', make: 'Toyota', year: 2020 },
-    { name: 'Honda', model: 'Accord', make: 'Honda', year: 2019 },
-    { name: 'Ford', model: 'Mustang', make: 'Ford', year: 2022 },
-    { name: 'Tesla', model: 'Model S', make: 'Tesla', year: 2023 },
-    { name: 'Chevrolet', model: 'Malibu', make: 'Chevrolet', year: 2021 },
-    { name: 'Nissan', model: 'Altima', make: 'Nissan', year: 2020 },
-    { name: 'Hyundai', model: 'Elantra', make: 'Hyundai', year: 2022 }]
+    {  id:1, name: 'Mercedes', model: 'C-Class', make: 'Mercedes-Benz', year: 2022 },
+    {   id:2,name: 'BMW', model: '3 Series', make: 'BMW', year: 2021 },
+    {   id:3,name: 'Audi', model: 'A4', make: 'Audi', year: 2023 },
+    {   id:4,name: 'Toyota', model: 'Camry', make: 'Toyota', year: 2020 },
+    {   id:5,name: 'Honda', model: 'Accord', make: 'Honda', year: 2019 },
+    {   id:6,name: 'Ford', model: 'Mustang', make: 'Ford', year: 2022 },
+    {   id:7,name: 'Tesla', model: 'Model S', make: 'Tesla', year: 2023 },
+    {   id:8,name: 'Chevrolet', model: 'Malibu', make: 'Chevrolet', year: 2021 },
+    {   id:9,name: 'Nissan', model: 'Altima', make: 'Nissan', year: 2020 },
+    {   id:10,name: 'Hyundai', model: 'Elantra', make: 'Hyundai', year: 2022 }]
 
 app.get('/',(req,res)=>
 {
@@ -22,12 +24,31 @@ app.get('/Car',(req,res)=>{
 })
 app.post('/AddCar',(req,res)=>
 {
-    res.send('Car Added');
+    const data=req.body;
+    Car.push(data);
+res.json({array:Car});
+    // res.send('Car Added');
 })
-app.put('/UpdateCar',(req,res)=>
+app.put('/UpdateCarparams/:id',(req,res)=>
     {
-        res.status(404).send('Car Update Success');
+        // res.status(404).send(req.query);
+      const req1=req.params.id;
+      if(re1)
+
     })
+    app.patch('/quereyCar/:model',(req,res)=>
+    {
+        const previousid=req.query.id;
+      const modelfind=Car.find(x=>x.model==previousmodel);
+      if(modelfind)
+      {
+        modelfind.name=req.body.name;
+        modelfind.model=req.body.model;
+        modelfind.make=req.body.make;
+        modelfind.year=req.body.year;
+        res.json({array:Car});
+      }
+    });
     app.delete('/DeleteCar',(req,res)=>
         {
             res.status(200).send('Car Deleted Success');
